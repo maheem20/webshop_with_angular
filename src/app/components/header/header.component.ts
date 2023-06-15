7f8331aa4e10b133d6077335863684fb966667b9
Maheem <88165826+maheem20@users.noreply.github.com>
2023-06-15 21:44:32 +0000

    add header component

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  template: `
+    <app-header></app-header>
+  `,
+  styles: [
+  ]
+})
+export class HeaderComponent {
+
+}

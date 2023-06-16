ad2b8701df33e68a8ba5b2b32a34740be6860eca
maheem_20 <mwistanbuly@gmail.com>
2023-06-16 23:43:36 +0200

    define html template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <router-outlet></router-outlet>
+    <app-header></app-header>
   `,
   styles: []
 })

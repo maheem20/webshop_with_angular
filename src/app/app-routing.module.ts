9f8104cda2ea21bdf361b0f3423160c6ee55b9f8
maheem_20 <mwistanbuly@gmail.com>
2023-06-19 21:36:12 +0200

    set default path to 'home'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,11 @@ import { HomeComponent } from './pages/home/home.component';
 const routes: Routes = [{
   path: 'home',
   component: HomeComponent
-}];
+},
+{
+  path: '', redirectTo: 'home', pathMatch: 'full'
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

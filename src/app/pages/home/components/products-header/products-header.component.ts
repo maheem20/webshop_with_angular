e3e9ec37a3dc7d70debc403844db9efe54b7779a
Maheem <88165826+maheem20@users.noreply.github.com>
2023-06-23 21:40:45 +0000

    create variable for items count

--- src/app/pages/home/components/products-header/products-header.component.ts
+++ src/app/pages/home/components/products-header/products-header.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsHeaderComponent implements OnInit {
   sort = 'Descending';
+  itemsShowCount = 12;
 
   constructor() { }
 

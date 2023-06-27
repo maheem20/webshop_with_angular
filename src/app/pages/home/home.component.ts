721a7f942fd3292216cf481aa77845540ba86ec8
maheem_20 <mwistanbuly@gmail.com>
2023-06-27 23:16:20 +0200

    remove styles

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,9 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html',
-  styles: [
-  ]
+  templateUrl: './home.component.html'
 })
 export class HomeComponent {
 

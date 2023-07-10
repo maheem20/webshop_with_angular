a5a3d5bfc80603555c0013cc06d7fdbdbca496d0
maheem_20 <mwistanbuly@gmail.com>
2023-07-10 22:26:09 +0200

    define constant object that maps row numbers to their corresponding height

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+const ROWS_HEIGHT: { [id: number]: number } = { 1: 400, 3: 335, 4: 350 };
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html'

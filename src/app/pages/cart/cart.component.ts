c9b4378e2bda67412710c2b4d7b6e4ceb8505b99
maheem_20 <mwistanbuly@gmail.com>
2023-07-15 23:49:40 +0200

    implement OnInit

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,10 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html'
 })
-export class CartComponent {
+export class CartComponent implements OnInit {
 
   constructor() { }
 

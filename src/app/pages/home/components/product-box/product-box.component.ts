13f5a5bf95626f4364c6d368034efbe84886a329
maheem_20 <mwistanbuly@gmail.com>
2023-07-25 23:01:34 +0200

    define a default product object

--- src/app/pages/home/components/product-box/product-box.component.ts
+++ src/app/pages/home/components/product-box/product-box.component.ts
@@ -9,6 +9,15 @@ export class ProductBoxComponent implements OnInit {
   [x: string]: any;
   @Input() fullWidthMode = false;
 
+  product: Product = {
+    id: 1,
+    title: 'Snearkers',
+    price: 150,
+    category: 'Shoes',
+    description: 'Description',
+    image: 'https://via.placeholder.com/150'
+  };
+
   constructor() { }
 
   ngOnInit(): void { }

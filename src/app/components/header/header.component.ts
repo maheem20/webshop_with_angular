3612f4dee86806098f83f6b5a3057ebd99e12dff
maheem_20 <mwistanbuly@gmail.com>
2023-07-26 23:26:53 +0200

    declare private property to hold cart data & initialize it with empty cart value

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { Cart } from 'src/app/models/cart.model';
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html', 
+  templateUrl: './header.component.html',
   styles: [
   ]
 })
 export class HeaderComponent {
+  private _cart: Cart = { items: [] };
 
 }

468cee00f40bbb6ad86eb3995160cfad3a5f78d1
maheem_20 <mwistanbuly@gmail.com>
2023-09-18 23:59:30 +0200

    subscribe to changes in cartService's cart object update local cart object with new cart data update dataSource property with new items in cart

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -32,6 +32,10 @@ export class CartComponent implements OnInit {
 
   ngOnInit(): void {
     this.dataSource = this.cart.items;
+    this.cartService.cart.subscribe((_cart: Cart) => {
+      this.cart = _cart;
+      this.dataSource = _cart.items;
+    });
   }
 
   getTotal(items: Array<CartItem>): number {

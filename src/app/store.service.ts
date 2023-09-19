769133c58cb72005360be7130a37064da716a203
Maheem <88165826+maheem20@users.noreply.github.com>
2023-09-19 22:02:48 +0000

    retrieves all products from store API

--- src/app/store.service.ts
+++ src/app/store.service.ts
@@ -1,10 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+const STORE_BASE_URL = 'http://fakestoreapi.com';
+
 @Injectable({
   providedIn: 'root'
 })
 export class StoreService {
 
   constructor(private httpClient: HttpClient) { }
+
+  getAllProducts(limit = 12, sort = 'desc'): Observable<Array<Product>> {
+    return this.httpClient.get<Array<Product>>(
+      `${STORE_BASE_URL}/products?sort=${sort}&limit=${limit}`
+    );
+  }
 }

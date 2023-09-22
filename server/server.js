0a1d830f19cc8fe31c1253fc63728030246412a4
Maheem <88165826+maheem20@users.noreply.github.com>
2023-09-22 21:45:02 +0000

    initialize Stripe with secret key

--- server/server.js
+++ server/server.js
@@ -7,3 +7,5 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors({ origin: true, credentials: true }));
+
+const stripe = require('stripe')('secret_key');     // add secret key here

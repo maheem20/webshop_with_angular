0c467cb2035bdb694221923aeda200a7dcdbe06d
Maheem <88165826+maheem20@users.noreply.github.com>
2023-09-22 21:43:58 +0000

    parse incoming request bodies in middleware before handlers

--- server/server.js
+++ server/server.js
@@ -4,3 +4,5 @@ const bodyParser = require('body-parser');
 
 const app = express();
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());

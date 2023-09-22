1f136e63a64a1a9c3cfc93acdb22e5f4147493a9
Maheem <88165826+maheem20@users.noreply.github.com>
2023-09-22 21:48:12 +0000

    create new Stripe checkout session

--- server/server.js
+++ server/server.js
@@ -10,4 +10,20 @@ app.use(cors({ origin: true, credentials: true }));
 
 const stripe = require('stripe')('secret_key');     // add secret key here
 
-app.post('/checkout', async (req, res) => { });
+app.post('/checkout', async (req, res) => {
+    try{
+        const session = await stripe.checkout.sessions.create({
+            line_items: req.body.line_items.map((item) => ({
+                currency: 'usd',
+                product_data: {
+                    name: item.name,
+                    images: [item.product]
+                },
+                unit_amount: item.price * 100,
+            })),
+            mode: 'payment',
+            success_url: 'http://localhost:4242/success.html',
+            cancel_url: 'http://localhost:4242/cancel.html',
+        });
+    }
+});

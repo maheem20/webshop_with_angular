3787990b32c288f9022b48e8ef5698b7f9c58e87
Maheem <88165826+maheem20@users.noreply.github.com>
2023-09-22 21:44:28 +0000

    enable CORS for all routes

--- server/server.js
+++ server/server.js
@@ -6,3 +6,4 @@ const app = express();
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors({ origin: true, credentials: true }));
